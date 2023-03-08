bffef94205e76af153aac0f6d24dc858a7a34fa2
Dalila Castro <dalilaccastro@gmail.com>
2023-03-08 17:55:07 -0300

    Created users.js. Modified UserList.js

---
+++ src/data/users.js
@@ -0,0 +1,62 @@
+export default [
+    {
+        id: 1,
+        name: 'Pedro Lima',
+        email: 'pedropl@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2020/11/22/22/17/male-5768177_960_720.png'
+    },
+    {
+        id: 2,
+        name: 'Maria Manera',
+        email: 'manera_marie@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2016/09/22/16/38/avatar-1687700_960_720.jpg'
+    },
+    {
+        id: 3,
+        name: 'João Gomes',
+        email: 'joao_123@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+    {
+        id: 4,
+        name: 'Ana Arlete',
+        email: 'arlete_ana@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2021/05/10/08/00/woman-6242836_960_720.png'
+    },
+    {
+        id: 5,
+        name: 'Indira Santos',
+        email: 'indira_c_santos@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: 6,
+        name: 'Ivo Mota',
+        email: 'ivomota42@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2020/11/22/22/17/male-5768177_960_720.png'
+    },
+    {
+        id: 7,
+        name: 'Neide Altiva',
+        email: 'altiva_n@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png'
+    },
+    {
+        id: 8,
+        name: 'Indira Silva',
+        email: 'indira_silva@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: 9,
+        name: 'Igor Mota',
+        email: 'igormota43@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2020/11/22/22/17/male-5768177_960_720.png'
+    },
+    {
+        id: 10,
+        name: 'Neide Arlete',
+        email: 'arlete_nene@emptymail.com',
+        avatar_url: 'https://cdn.pixabay.com/photo/2017/04/30/11/42/hijab-2272708_960_720.png'
+    }
+]
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,8 +1,65 @@
 import React from "react";
-import { Text } from 'react-native'
+import { View, FlatList, Alert } from 'react-native'
+import users from "../data/users";
+import { ListItem, Avatar } from '@rneui/themed'
+import { Button, Icon } from "@rneui/base";
 
 export default props => {
+    function confirmUserDeletion(user) {
+        Alert.alert('Excluir Usuário', 'Deseja realmente excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress(){
+                    console.warn('deletou' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
+
+
+    function getActions(user){
+        return(
+            <>
+                <Button
+                    onPress={()=> props.navigation.navigate('UserForm', user)}
+                    type="clear"
+                    icon={<Icon name="edit" size={25} color="orange" />}
+                />
+                <Button
+                    onPress={()=> confirmUserDeletion(user)}
+                    type="clear"
+                    icon={<Icon name="delete" size={25} color="red" />}
+                />
+            </>
+        )
+    }
+
+    function getUserItem({ item: user }) {
+        return (
+            <ListItem.Swipeable
+                key={user.id}
+                bottomDivider
+                onPress={() => props.navigation.navigate('UserForm', user)}>
+            <Avatar rounded source={{uri: user.avatar_url}}/>
+            <ListItem.Content>
+                <ListItem.Title>{user.name}</ListItem.Title>
+                <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+            </ListItem.Content>
+                {getActions(user)}
+            </ListItem.Swipeable>    
+      )
+    }
+
     return (
-        <Text>UserList</Text>
+        <View>
+           <FlatList 
+                keyExtractor={user => user.id.toString()}
+                data={users}
+                renderItem={getUserItem}
+           /> 
+        </View>
     )
 }
